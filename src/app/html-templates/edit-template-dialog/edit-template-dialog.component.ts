3b667ddd1528d21a1dfbb14b13b3c6fbc5148e04
Justin Beattie <justinbeattie@Justins-MacBook-Pro.local>
2020-04-24 11:57:46 +1000

    tweaks

--- src/app/html-templates/edit-template-dialog/edit-template-dialog.component.ts
+++ src/app/html-templates/edit-template-dialog/edit-template-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, ViewChild} from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HtmlTemplatesComponent, DialogData } from '../html-templates.component';
 import { MatMenuTrigger } from '@angular/material/menu';
@@ -19,6 +19,16 @@ export class EditTemplateDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
   ngOnInit(): void {
+  }
+
+
+  changeFont() {
+    if (this.data.currentTemplate.label.position == 'top' || this.data.currentTemplate.label.position == 'bottom') {
+      this.data.currentTemplate.label.font = "Inter";
+    } else {
+      this.data.currentTemplate.label.font = "icons_text";
+    }
+   console.log(this.data.currentTemplate);
   }
 
   addColor() {
